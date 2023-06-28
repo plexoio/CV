273580d8d4b85ea117ebe977093343405d67242d
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2023-06-28 16:31:44 +0000

    feat: finish Google Maps & Markers integration start Github

--- vendor/js/google_maps.js
+++ vendor/js/google_maps.js
@@ -1,14 +1,26 @@
-
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 15,
         center: {
-            lat: 44,
-            lng: -33
+            lat: 50.112400,
+            lng: 8.668600
         }
-    }
-    );
+    });
 
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 50.112400, lng: 8.668600 },
+    ];
+
+    var markers = locations.map(function (location, indexing) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[indexing % labels.length],
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
 
 window.initMap = initMap;
